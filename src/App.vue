af963d88abddcdfbbf0a57c945c865269f019a86
ikhsanansari <ikhsanansari11@gmail.com>
2024-05-27 09:24:58 +0700

    tugas pbk 5

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,119 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-secondary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="src/assets/logo.jpg">
+          </q-avatar>
+          Tokoku
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Home" />
+        <q-route-tab to="/page2" label="About" />
+        <q-route-tab to="/page3" label="Lainnya" />
+      </q-tabs>
+    </q-header>
+
+    
+
+    <q-page-container>
+      <router-view />
+      <div class="q-pa-md">
+    <div class="q-gutter-md">
+      <q-carousel
+        v-model="slide"
+        transition-prev="scale"
+        transition-next="scale"
+        swipeable
+        animated
+        control-color="white"
+        navigation
+        padding
+        arrows
+        height="300px"
+        class="bg-primary text-white shadow-1 rounded-borders"
+      >
+        <q-carousel-slide name="style" class="column no-wrap flex-center">
+          <q-icon name="style" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="tv" class="column no-wrap flex-center">
+          <q-icon name="live_tv" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="layers" class="column no-wrap flex-center">
+          <q-icon name="layers" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="map" class="column no-wrap flex-center">
+          <q-icon name="terrain" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+
+      
+     
+        
+       
+     
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            
+          </q-avatar>
+          <div class="footer" >instagram: @Tokokulaku</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const rightDrawerOpen = ref(false)
+
+    return {
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+  }
+
+
+
+</script>
+
+<style>
+body {
+  font-family: 'Poppins', sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+.footer {
+  text-align: center;
+  font-size: 15px;
+  }
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+<template>
+ <div id="app">
+      <Navbar />
+      <q-layout view="hHh lpR fFf">
+        <q-page-container>
+          <router-view />
+        </q-page-container>
+        <q-footer elevated class="bg-grey-8 text-white">
+          <q-toolbar>
+            <q-toolbar-title>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+              </q-avatar>
+              <div>Title</div>
+            </q-toolbar-title>
+          </q-toolbar>
+        </q-footer>
+      </q-layout>
+    </div>
+
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+  
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+  </template>
+  
+ 
+  
+  <style scoped>
+  /* Tambahkan gaya CSS khusus untuk komponen navbar di sini */
+  </style>
+  
